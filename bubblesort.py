2fa3d5789731e4d24703c203b728dfa2b55d8239
rajuf6 <rajupanugantif6@gmail.com>
2020-09-01 00:35:06 +0530

    first commit

---
+++ bubblesort.py
@@ -0,0 +1,11 @@
+def sort(nums):
+    for i in range(len(nums)-1,0,-1):
+        for j in range(i):
+            if nums[j]>nums[j+1]:
+                temp = nums[j]
+                nums[j]=nums[j+1]
+                nums[j+1]=temp
+
+nums = [5,1,4,9,6,2]
+sort(nums)
+print(nums) 
