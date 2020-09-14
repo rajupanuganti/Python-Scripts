6107af8043fd7649cc0a888e6e282faeec576bf3
rajuf6 <rajupanugantif6@gmail.com>
2020-09-14 22:57:26 +0530

    first commit

---
+++ function.py
@@ -0,0 +1,6 @@
+def add_sub(x,y):
+    c=x+y
+    d=x-y
+    return c,d 
+res1,res2 = add_sub(9,7)
+print(res1,res2)
---
+++ test.py
@@ -0,0 +1,4 @@
+from numpy import*
+arr = array([12,45,6,9])
+arr1 = array([45,78,5,47])
+print(concatenate([arr,arr1]))
